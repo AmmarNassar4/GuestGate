432012e456d1084d82d5d30a09f42e11e70f0fdc
AmmarNassar4 <ammar.nassar@outlook.com>
2026-06-01 21:15:08 +0300

    Remove guest save workaround from DbContext

--- src/GuestGate.Api/Data/AppDb.cs
+++ src/GuestGate.Api/Data/AppDb.cs
@@ -1,7 +1,5 @@
 using GuestGate.Api.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage;
 namespace GuestGate.Api.Data
 {
     public class AppDb : DbContext
@@ -32,43 +30,6 @@ namespace GuestGate.Api.Data
 
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
-            var pendingGuestLinks = ChangeTracker.Entries<KioskSession>()
-                .Where(e => (e.State == EntityState.Added || e.State == EntityState.Modified) && e.Entity.GuestId == 0)
-                .ToList();
-
-            var addedGuests = ChangeTracker.Entries<Guest>()
-                .Where(e => e.State == EntityState.Added)
-                .Select(e => e.Entity)
-                .ToList();
-
-            if (pendingGuestLinks.Count > 0 && addedGuests.Count == 1)
-            {
-                ApplyAuditTimestamps();
-
-                var sessionStates = pendingGuestLinks
-                    .Select(e => new { Entry = e, State = e.State })
-                    .ToList();
-
-                foreach (var sessionEntry in pendingGuestLinks)
-                {
-                    sessionEntry.State = EntityState.Unchanged;
-                }
-
-                var insertedGuests = await base.SaveChangesAsync(cancellationToken);
-
-                foreach (var sessionState in sessionStates)
-                {
-                    sessionState.Entry.Entity.GuestId = addedGuests[0].Id;
-                    sessionState.Entry.State = sessionState.State == EntityState.Added
-                        ? EntityState.Added
-                        : EntityState.Modified;
-                }
-
-                ApplyAuditTimestamps();
-                var linkedSessions = await base.SaveChangesAsync(cancellationToken);
-                return insertedGuests + linkedSessions;
-            }
-
             ApplyAuditTimestamps();
             return await base.SaveChangesAsync(cancellationToken);
         }
