9cc4d0a202a9f94b5f453e969a8bc26dbe53cdfd
AmmarNassar4 <ammar.nassar@outlook.com>
2026-06-03 02:17:48 +0300

    Convert Kid DTO fields to int

--- src/GuestGate.Api/Models/DTOs.cs
+++ src/GuestGate.Api/Models/DTOs.cs
@@ -2,8 +2,8 @@ using System;
 using System.Text.Json;
 namespace GuestGate.Api.Models
 {
-    public record SessionStartDto(string kid, string? templateId, JsonElement? prefill);
+    public record SessionStartDto(int kid, string? templateId, JsonElement? prefill);
     public record MobileSaveDto(Guid et, JsonElement data);
-    public record ConsentCreateDto(string kid, string? guestName, string? identityNumber, string? language, string? checkInTime, string? termsEn, string? termsAr);
+    public record ConsentCreateDto(int kid, string? guestName, string? identityNumber, string? language, string? checkInTime, string? termsEn, string? termsAr);
     public record ConsentSignDto(bool accepted, string signatureImage, string? language);
 }
