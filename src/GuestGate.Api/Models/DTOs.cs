8370149383fa1c67089ffb81b496d72ba0fd0e37
AmmarNassar4 <ammar.nassar@outlook.com>
2026-06-10 12:31:46 +0300

    Allow flexible consent Kid JSON values

--- src/GuestGate.Api/Models/DTOs.cs
+++ src/GuestGate.Api/Models/DTOs.cs
@@ -4,6 +4,6 @@ namespace GuestGate.Api.Models
 {
     public record SessionStartDto(int kid, string? templateId, JsonElement? prefill);
     public record MobileSaveDto(Guid et, JsonElement data);
-    public record ConsentCreateDto(int kid, string? guestName, string? identityNumber, string? language, string? checkInTime, string? termsEn, string? termsAr);
+    public record ConsentCreateDto(JsonElement kid, string? guestName, string? identityNumber, string? language, string? checkInTime, string? termsEn, string? termsAr);
     public record ConsentSignDto(bool accepted, string signatureImage, string? language);
 }
